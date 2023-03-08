2933fc28855a85168c4ed9c0dc16384a534c7116
Krystian <texen24@gmail.com>
2023-03-08 20:04:36 +0100

    ended project

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 const navMobile = document.querySelector('.nav-mobile')
 const navBtn = document.querySelector('.hamburger')
 const footerYear = document.querySelector('.footer__year')
-
-
-
+const allNavItems = document.querySelectorAll('.nav__link')
 
 const handleNav = () => {
-	navBtn.classList.toggle('is-active');
-	navMobile.classList.toggle('nav-mobile--active');
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
 
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('nav-mobile--active')
+			navBtn.classList.remove('is-active')
+		})
+	})
 }
 
-
-
-
 navBtn.addEventListener('click', handleNav)
+allNavItems.addEventListener('click', handleLink)
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
